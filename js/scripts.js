3b9803c924709825bfb0786577c43d0d1fa1b2d3
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-09-04 19:57:36 -0400

    Generated a questionaire that calculates score based on correct answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,27 @@
 //welcome the user
 alert("Welcome to Quiz Ninja!");
+
+var question = "What is Superman's real name?"
+var answer = prompt(question);
+alert("You answered " + answer);
+
+var quiz = [
+  ["What is Superman's real name?", "Clarke Kent"],
+  ["What is Wonderwoman's real name?", "Dianna Prince"],
+  ["What is Batman's real name?", "Bruce Wayne"]
+];
+
+var score = 0 // initialize score
+
+for(var i=0,max=quiz.length;i<max;i++){
+  var answer = prompt(quiz[i][0]);
+
+  if(answer === quiz[i][1]){
+    alert("Correct!");
+    score++
+  } else {
+    alert("Wrong!");
+  }
+}
+
+alert("Game Over, you scored " + score + " points")
