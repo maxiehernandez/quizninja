3ebcf4e0e925dbc5674405da39ebb3de43fc8727
Maxie Hernandez <maxinehernandez@hotmail.com>
2016-09-04 20:30:43 -0400

    Refactored to add functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
-//welcome the user
-alert("Welcome to Quiz Ninja!");
-
-var question = "What is Superman's real name?"
-var answer = prompt(question);
-alert("You answered " + answer);
-
 var quiz = [
-  ["What is Superman's real name?", "Clarke Kent"],
-  ["What is Wonderwoman's real name?", "Dianna Prince"],
-  ["What is Batman's real name?", "Bruce Wayne"]
-];
-
+    ["What is Superman's real name?","Clarke Kent"],
+    ["What is Wonderwoman's real name?","Dianna Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+  ];
 var score = 0 // initialize score
+play(quiz);
 
-for(var i=0,max=quiz.length;i<max;i++){
-  var answer = prompt(quiz[i][0]);
+function play(quiz){
+  for(var i=0, question, answer, max=quiz.length; i<max; i++) {
+    question = quiz[i][0];
+    answer = ask(question);
+    check(answer);
+  }
+  gameOver();
 
-  if(answer === quiz[i][1]){
-    alert("Correct!");
-    score++
-  } else {
-    alert("Wrong!");
+  function ask(question) {
+    return prompt(question);
+  }
+
+  function check(answer) {
+    if(answer === quiz[i][1]){
+      alert("Correct!");
+      score++;
+    } else {
+      alert("Wrong!");
+    }
+  }
+
+  function gameOver(){
+    alert("Game Over, you scored " + score + " points");
   }
 }
-
-alert("Game Over, you scored " + score + " points")
